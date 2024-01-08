239c95e489b17fe456193346893d54ca20bb28aa
Ryan Wohler <ryanwohler55@gmail.com>
2024-01-08 10:59:23 -0400

    Simple Patrol

---
+++ Assets/Ryan/RWScripts/GhostBehavior.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GhostBehavior : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+    public Transform[] patrolPoints;
+    public Transform currentPatrolPoint;
+    public int curIndex;
+
+    public float distToSwitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchToPoint(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(distToSwitch > Vector3.Distance(transform.position, currentPatrolPoint.position))
+        {
+            curIndex++;
+            if(curIndex >= patrolPoints.Length) {
+                curIndex = 0;
+            }
+            SwitchToPoint(curIndex);
+        }
+    }
+
+    void SwitchToPoint(int index)
+    {
+        if (index < patrolPoints.Length)
+        {
+            currentPatrolPoint = patrolPoints[index];
+            agent.SetDestination(currentPatrolPoint.position);
+            curIndex = index;
+        }
+    }
+
+}
