a3b5d7accdce06f2ef1cd9bcc6b9dc7001cfbfbb
Ryan Wohler <ryanwohler55@gmail.com>
2024-01-18 13:49:11 -0400

    float trigger naming conventions and ghost adjustments

--- Assets/Scripts/FloatTrigger.cs
+++ Assets/Scripts/FloatTrigger.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class FloatTrigger : MonoBehaviour
 {
     //The amount of force applied to floating object
-    public float floatForce;
+    public float flt_floatForce;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider cl_other)
     {
         //Make Float Object stop having gravity, and push it upwards
-        TestFloatObject obj = other.GetComponent<TestFloatObject>();
+        TestFloatObject obj = cl_other.GetComponent<TestFloatObject>();
         if (obj != null)
         {
             Rigidbody rb = obj.gameObject.GetComponent<Rigidbody>();
@@ -16,16 +16,16 @@ public class FloatTrigger : MonoBehaviour
             {
                 obj.isFloating = true;
                 rb.useGravity = false;
-                rb.AddForce(Vector3.up * floatForce);
+                rb.AddForce(Vector3.up * flt_floatForce);
             }
             GameManager.ghost.l_go_throwables.Add(obj.gameObject);
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider cl_other)
     {
         //Give Float Object it's gravity back
-        TestFloatObject obj = other.GetComponent<TestFloatObject>();
+        TestFloatObject obj = cl_other.GetComponent<TestFloatObject>();
         if (obj != null)
         {
             Rigidbody rb = obj.gameObject.GetComponent<Rigidbody>();
