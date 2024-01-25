81ea1ba2a4d8539d2b7e53cb786e6645e9721051
Rowan Martel <rowanmartel@gmail.com>
2024-01-25 15:15:35 -0400

    removed unecessary debug logs

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -10,11 +10,7 @@ public class SoundManager : MonoBehaviour
 
     public void PlayClip(AudioClip ac_clip, AudioSource as_source)
     {
-        //Debug.Log(l_ac_curSFX.Contains(ac_clip));
         if (l_ac_curSFX.Contains(ac_clip)) return;
-
-        //foreach (AudioClip ac in l_ac_curSFX)
-        //    if (ac.name == ac_clip.name) return;
 
         Vector3 v3_sourcePos = as_source.transform.position;
         if (Vector3.Distance(GameManager.playerController.transform.position, v3_sourcePos) >= Settings.int_SFXCullingDist)
@@ -31,8 +27,6 @@ public class SoundManager : MonoBehaviour
     IEnumerator RemoveClip(float flt_waitTime, AudioClip cl)
     {
         yield return new WaitForSeconds(flt_waitTime);
-        Debug.Log("A");
         l_ac_curSFX.Remove(cl);
-        Debug.Log(l_ac_curSFX.Contains(cl));
     }
 }
