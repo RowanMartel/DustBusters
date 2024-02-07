75ce0d290f1374ffe000c21ff1fb2fd2a44dec8a
Chris Schnurr <ctschnurr@hotmail.com>
2024-02-07 12:07:07 -0400

    Made a start on opening the drawer functionality

---
+++ Assets/Scripts/DrawerOpen.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerOpen : Interactable
+{
+    public enum Direction
+    {
+        xUP,
+        xDOWN,
+        zUP,
+        zDOWN
+    }
+
+    public Direction en_openDirection;
+    public bool bl_open;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        Toggle();
+    }
+
+    void Toggle()
+    {
+
+    }
