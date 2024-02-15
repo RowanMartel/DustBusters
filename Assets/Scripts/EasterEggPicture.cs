fc2f07fc5f54b3f2b4496b56fe43c277cf99acd3
Ryan Wohler <ryanwohler55@gmail.com>
2024-02-15 10:57:01 -0400

    Reworked photos so that they don't glow anymore

--- Assets/Scripts/EasterEggPicture.cs
+++ Assets/Scripts/EasterEggPicture.cs
@@ -5,16 +5,29 @@ using UnityEngine.UI;
 
 public class EasterEggPicture : MonoBehaviour
 {
-    public Image img_image;
+    //public Image img_image;
     public Sprite spt_normal;
     public Sprite spt_after;
+    public Material mat_material;
+
+    private void Start()
+    {
+        mat_material = gameObject.GetComponent<Renderer>().material;
+        mat_material.mainTexture = spt_normal.texture;
+    }
 
     //Swap image for easter egg image
     public void Switch()
     {
-        if(img_image.sprite != spt_after)
+        //if(img_image.sprite != spt_after)
+       // {
+        //    img_image.sprite = spt_after;
+        //}
+
+        if(mat_material.mainTexture != spt_after.texture)
         {
-            img_image.sprite = spt_after;
+            mat_material.mainTexture = spt_after.texture;
         }
+
     }
 }
