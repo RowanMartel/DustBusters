0e89008134b352b40f61f9896c1fb7ff5c9adc10
Ryan Wohler <ryanwohler55@gmail.com>
2024-02-15 11:14:24 -0400

    Removed unnecessary start and update from book

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -5,16 +5,4 @@ using UnityEngine;
 public class Book : MonoBehaviour
 {
     [HideInInspector] public bool bl_onShelf;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
