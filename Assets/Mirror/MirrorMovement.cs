4cf31b95df094801c9e65b5faec341c240b6413a
Ryan Wohler <ryanwohler55@gmail.com>
2024-03-27 14:34:43 -0300

    Trying to push mirror changes

--- Assets/Mirror/MirrorMovement.cs
+++ Assets/Mirror/MirrorMovement.cs
@@ -11,25 +11,61 @@ public class MirrorMovement : MonoBehaviour
 
     public GameObject[] a_go_regions;
 
+    bool bl_active;
+    public int int_framesToRend;
+    int cur_framesToRend;
+    Camera cam;
+    public GameObject go_renderObj;
+    public Renderer ren_renderer;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
     void Update()
     {
-        Vector3 v3LocalPlayer = tfMirror.InverseTransformPoint(tfPlayerTarget.position);
-        //transform.position = tfMirror.TransformPoint(new Vector3(v3LocalPlayer.x, -v3LocalPlayer.y, v3LocalPlayer.z));
+        if (ren_renderer.isVisible)
+        {
+            go_renderObj.SetActive(true);
+        }
+        else
+        {
+            go_renderObj.SetActive(false);
+        }
 
-        Vector3 v3LookAtMirror = tfMirror.TransformPoint(new Vector3(intXMod * v3LocalPlayer.x, intYMod * v3LocalPlayer.y, intZMod * v3LocalPlayer.z));
-        transform.LookAt(v3LookAtMirror);
+        if(bl_active)
+        {
+            cur_framesToRend++;
+            if(cur_framesToRend >= int_framesToRend)
+            {
+                cur_framesToRend = 0;
+                if (ren_renderer.isVisible)
+                {
+                    Vector3 v3LocalPlayer = tfMirror.InverseTransformPoint(tfPlayerTarget.position);
+                    //transform.position = tfMirror.TransformPoint(new Vector3(v3LocalPlayer.x, -v3LocalPlayer.y, v3LocalPlayer.z));
 
-        //GetComponent<Camera>().nearClipPlane = Vector3.Distance(transform.position, tfMirror.position) + 1;
+                    Vector3 v3LookAtMirror = tfMirror.TransformPoint(new Vector3(intXMod * v3LocalPlayer.x, intYMod * v3LocalPlayer.y, intZMod * v3LocalPlayer.z));
+                    transform.LookAt(v3LookAtMirror);
+
+                    cam.Render();
+
+                    //GetComponent<Camera>().nearClipPlane = Vector3.Distance(transform.position, tfMirror.position) + 1;
+                }
+            }
+        }
     }
 
     public void ActivateCam()
     {
-        gameObject.SetActive(true);
+        bl_active = true;
+        //gameObject.SetActive(true);
     }
 
     public void DeactivateCam()
     {
-        gameObject.SetActive(false);
+        bl_active = false;
+        //gameObject.SetActive(false);
     }
 
 }
