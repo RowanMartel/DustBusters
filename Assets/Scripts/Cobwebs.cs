436a81bbf12aa7f3022662177a85f0d981a1ddd4
Ryan Wohler <ryanwohler55@gmail.com>
2024-04-02 10:54:48 -0300

    Updated TaskManager for multiple chores

--- Assets/Scripts/Cobwebs.cs
+++ Assets/Scripts/Cobwebs.cs
@@ -4,11 +4,13 @@ public class Cobwebs : MonoBehaviour
 {
     [Tooltip("Put the amount of cobwebs the player needs to clean here")]
     public int int_cobwebs;
+    public int int_startingCobwebs;
 
     // ticks down cobwebs int, then ends the task if complete
     public void CleanWeb()
     {
         int_cobwebs--;
+        GameManager.taskManager.UpdateTask(int_startingCobwebs - int_cobwebs, TaskManager.Task.CleanCobwebs);
         if (int_cobwebs <= 0)
             GameManager.taskManager.CompleteTask(TaskManager.Task.CleanCobwebs);
     }
