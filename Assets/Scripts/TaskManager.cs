a1362982df9d858eb77916b322b9404fc09edac3
Ryan Wohler <ryanwohler55@gmail.com>
2024-04-04 13:54:37 -0300

    Removed code related to unused task UI

--- Assets/Scripts/TaskManager.cs
+++ Assets/Scripts/TaskManager.cs
@@ -33,7 +33,6 @@ public class TaskManager : MonoBehaviour
     [HideInInspector] public List<Task> li_tsk_completedTaskList;
 
     public GhostBehavior ghost;
-    public TMP_Text tmp_taskListTxt;
 
     // events to notify Menu Manager of chore update and complete
     public event EventHandler<EventArgs> ChoreCompleted;
@@ -107,8 +106,6 @@ public class TaskManager : MonoBehaviour
         ghost = GameManager.ghost;
 
         li_taskList = new();
-        tmp_taskListTxt = FindObjectOfType<EmptyTaskList>(true).GetComponent<TMP_Text>();
-        tmp_taskListTxt.text = "";
 
         li_tsk_completedTaskList.Clear();
 
@@ -154,52 +151,6 @@ public class TaskManager : MonoBehaviour
                 return;
             }
         }
-
-        string text = "";
-        switch (task)
-        {
-            case Task.CleanDishes:
-                text = "\nClean the dirty dishes";
-                break;
-            case Task.PutAwayDishes:
-                text = "\nPut away the dishes on the counter";
-                break;
-            case Task.CleanCobwebs:
-                text = "\nClean away the cobwebs in the foyer";
-                break;
-            case Task.CleanMirror:
-                text = "\nClean the bathroom mirror";
-                break;
-            case Task.ThrowOutBrokenDishes:
-                text = "\nThrow out the broken dishes";
-                break;
-            case Task.EscapeHouse:
-                text = "\nEscape the house";
-                break;
-            case Task.FindKey:
-                text = "\nFind the front door key";
-                break;
-            case Task.LightFireplace:
-                text = "\nLight the fireplace";
-                break;
-            case Task.MopFloor:
-                text = "\nMop the laundry room floor";
-                break;
-            case Task.PutAwayBooks:
-                text = "\nPut the books back on the shelf";
-                break;
-            case Task.ResetBreakerBox:
-                text = "\nReset the breaker box";
-                break;
-            case Task.PutAwayToys:
-                text = "\nPut away the toys";
-                break;
-        }
-        int i = tmp_taskListTxt.text.IndexOf(text);
-        //tmp_taskListTxt.text = tmp_taskListTxt.text.Remove(i, text.Length);
-
-        //Adds a strikethrough to a completed task
-        //tmp_taskListTxt.text = tmp_taskListTxt.text.Insert(i, "<s>" + text + "</s>");
 
         li_taskList.Remove(task);
         li_tsk_completedTaskList.Add(task);
@@ -265,66 +216,15 @@ public class TaskManager : MonoBehaviour
     {
         if (li_taskList.Contains(task)) return;
         li_taskList.Add(task);
-        string str_text = "";
-        switch (task)
-        {
-            case Task.CleanDishes:
-                str_text = "\nClean the dirty dishes in the kitchen";
-                break;
-            case Task.PutAwayDishes:
-                str_text = "\nPut away the clean dishes in the kitchen";
-                break;
-            case Task.CleanCobwebs:
-                str_text = "\nClean away the cobwebs in the foyer";
-                break;
-            case Task.CleanMirror:
-                str_text = "\nClean the bathroom mirror";
-                break;
-            case Task.ThrowOutBrokenDishes:
-                str_text = "\nThrow out the broken dishes";
-                break;
-            case Task.EscapeHouse:
-                str_text = "\nEscape the house";
-                break;
-            case Task.FindKey:
-                str_text = "\nFind the front door key";
-                break;
-            case Task.LightFireplace:
-                str_text = "\nLight the fireplace";
-                break;
-            case Task.MopFloor:
-                str_text = "\nMop the laundry room floor";
-                break;
-            case Task.PutAwayBooks:
-                str_text = "\nPut the books back on the shelf";
-                break;
-            case Task.ResetBreakerBox:
-                str_text = "\nReset the breaker box in the basement";
-                break;
-            case Task.PutAwayToys:
-                str_text = "\nPut away the toys";
-                break;
-        }
+
         if (a_int_timesCompletedTasks[(int)task] > 0)
         {
             a_int_timesCompletedTasks[(int)task]--;
         }
         if (li_tsk_completedTaskList.Contains(task))
         {
-            //Remove strikethrough from an uncompleted task
-            string str_strikethroughText = "<s>" + str_text + "</s>";
-
-            int i = tmp_taskListTxt.text.IndexOf(str_strikethroughText);
-
-            //tmp_taskListTxt.text = tmp_taskListTxt.text.Remove(i, str_strikethroughText.Length);
-
-            //tmp_taskListTxt.text = tmp_taskListTxt.text.Insert(i, str_text);
 
             li_tsk_completedTaskList.Remove(task);
-        }
-        else
-        {
-            //tmp_taskListTxt.text += str_text;
         }
 
         if (bl_taskListFilled)
@@ -366,7 +266,7 @@ public class TaskManager : MonoBehaviour
                         chore.tmp_choreText.text = "Dust the cobwebs in the basement";
                         break;
                     case Task.CleanMirror:
-                        chore.tmp_choreText.text = "Clean the downstairs bathroom mirror";
+                        chore.tmp_choreText.text = "Clean the bathroom mirrors";
                         break;
                     case Task.ThrowOutBrokenDishes:
                         chore.tmp_choreText.text = "Throw out the broken dishes";
