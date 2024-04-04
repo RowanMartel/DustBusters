17185abe1dc042eceb9fe0cb2958fb4ded2e3569
Ryan Wohler <ryanwohler55@gmail.com>
2024-04-04 11:00:57 -0300

    Removing books updates UI

--- Assets/Scripts/BookshelfTrigger.cs
+++ Assets/Scripts/BookshelfTrigger.cs
@@ -26,6 +26,7 @@ public class BookshelfTrigger : MonoBehaviour
         if(book != null)
         {
             libraryManager.RemoveBook(book);
+            libraryManager.CheckIfComplete();
         }
     }
 
